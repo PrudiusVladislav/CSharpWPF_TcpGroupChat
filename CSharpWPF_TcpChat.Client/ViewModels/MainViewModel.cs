d73c1e75e1e9b27e6024393a92981a4bcf01355e
Vlad Prudius <vladvlpr006@gmail.com>
2023-10-05 19:53:01 +0300

    Added some login and register xaml

--- CSharpWPF_TcpChat.Client/ViewModels/MainViewModel.cs
+++ CSharpWPF_TcpChat.Client/ViewModels/MainViewModel.cs
@@ -1,12 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Input;
-using CSharpWPF_TcpChat.Client.Infrastructure;
-using Microsoft.VisualBasic;
+
 using SharedComponents;
 
 namespace CSharpWPF_TcpChat.Client.ViewModels;
